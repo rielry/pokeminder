e25e6c74342e30c3f64d6b599eabad49034e6440
Riley <rileyjhi@Rileys-MacBook-Air.local>
2024-05-21 01:44:10 -0600

    Initial commit

---
+++ src/community-days.js
@@ -0,0 +1,81 @@
+import jsdom from "jsdom";
+
+const config = {
+  strings: {
+    communityDay: "community day",
+    incenseDay: "incense day",
+  },
+  months: [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ],
+  time: {
+    remider_time: 3 * 60 * 60 * 24 * 1000,
+  },
+  classNames: {
+    blogPosts: "blogList__posts",
+    blogPostContent: "blogList__post__content",
+    postDate: "blogList__post__content__date",
+    postTitle: "blogList__post__content__title",
+    post: {
+      headline: "ContainerBlock__headline__title",
+      body: "ContainerBlock__body",
+    },
+  },
+  url: "https://pokemongolive.com/news?hl=en",
+};
+
+const today = new Date();
+const currentMonth = today.getMonth() - 1;
+
+export const getUpcomingCommunityDays = async () => {
+  const document = (await jsdom.JSDOM.fromURL(config.url)).window.document;
+  const blogPosts = Array.from(
+    document.getElementsByClassName(config.classNames.blogPosts)[0].childNodes
+  ).filter((d) => d.hasChildNodes());
+  const communityDays = (
+    await Promise.all(blogPosts.map((post) => parseBlogPost(post)))
+  ).filter((d) => !!d);
+
+  return communityDays;
+};
+
+const parseBlogPost = async (post) => {
+  const title = post
+    .getElementsByClassName(config.classNames.postTitle)[0]
+    .innerHTML.trim();
+  const titleTokens = title.split(" ");
+  const month = titleTokens[0].trim();
+
+  const isUpcoming = config.months.indexOf(month) >= currentMonth;
+
+  if (isUpcoming && title.toLowerCase().includes(config.strings.communityDay)) {
+    const pokemon = titleTokens[titleTokens.length - 1].trim();
+    const dateAndTime = await getDateAndTime(post.href);
+    const date = new Date(dateAndTime.match(/[a-zA-Z]+ [0-9]{1,2}, [0-9]{4}/));
+
+    return {
+      date: date,
+      dateAndTime: dateAndTime,
+      title: title,
+      pokemon: pokemon,
+      url: post.href,
+    };
+  }
+};
+
+const getDateAndTime = async (url) => {
+  const document = (await jsdom.JSDOM.fromURL(url)).window.document;
+  return document.getElementsByClassName(config.classNames.post.body)[0]
+    .firstChild.innerHTML;
+};
---
+++ src/discordbot.js
@@ -0,0 +1,45 @@
+import dotenv from "dotenv";
+import { Client, IntentsBitField } from "discord.js";
+import schedule from "node-schedule";
+import { getUpcomingCommunityDays } from "./community-days.js";
+
+dotenv.config;
+
+const ONE_DAY = 24 * 60 * 60 * 1000;
+const today = new Date();
+
+const client = new Client({
+  partials: ["MESSAGE", "CHANNEL", "REACTION"],
+  intents: [IntentsBitField.Flags.Guilds, IntentsBitField.Flags.GuildMessages],
+});
+
+client.login(process.env.CLIENT_TOKEN);
+
+client.on("ready", async () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+
+  schedule.scheduleJob("30 9 * * *", async () => {
+    sendCommunityDayReminder();
+  });
+});
+
+
+const sendCommunityDayReminder = async () => {
+  const communityDays = await getUpcomingCommunityDays();
+  const upcomingTomorrow = communityDays.find(
+    (d) => d.date.valueOf() - today.valueOf() === ONE_DAY
+  );
+
+  if (upcomingTomorrow) {
+    const messageContent = `\n**Reminder**: ${upcomingTomorrow.pokemon} Community Day is tomorrow!\n${upcomingTomorrow.dateAndTime}\n\n${upcomingTomorrow.url}`;
+
+    const guilds = client.guilds.cache;
+    const keys = guilds.keys();
+
+    for (const key of keys) {
+      const guild = guilds.get(key);
+      const channel = guild.channels.cache.find((d) => d.name === "general");
+      channel.send({ content: messageContent });
+    }
+  }
+};
